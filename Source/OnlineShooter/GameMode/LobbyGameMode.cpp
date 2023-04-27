2d4e2c8296967d4e1ca77a10e65e8f683d795310
Ömer Ertekin <57711950+OmerErtekin@users.noreply.github.com>
2023-04-27 19:17:15 +0300

    require 1 player to play

--- Source/OnlineShooter/GameMode/LobbyGameMode.cpp
+++ Source/OnlineShooter/GameMode/LobbyGameMode.cpp
@@ -6,7 +6,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	Super::PostLogin(NewPlayer);
 	int32 numberOfPlayers = GameState.Get()->PlayerArray.Num();
 
-	if (numberOfPlayers == 2)
+	if (numberOfPlayers == 1)
 	{
 		UWorld* world = GetWorld();
 		if (world)
